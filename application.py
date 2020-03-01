0de0a938171da6388e36d5a29c98150e97d5e5dc
Chris Launey <clauney@gmail.com>
2020-03-01 13:49:19 -0800

    finished mvp, with adding movies and the basis for adding to lists, etc. now need to make different data paths for the different lists

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ Created on Sun Feb 16 17:56:08 2020
 import datetime
 import sys
 
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 
 import datawrangler
 
@@ -19,28 +19,58 @@ run_webapp = False
 
 
 #%% SCHEMA-Y THINGS
+
+
 attribute_defs = [
     {'id': 'name', 'name': 'Name', 'required': True, 'type': 'text'},
     {'id': 'year', 'name': 'Year', 'required': False, 'type': 'number'},
-    {'id': 'genre', 'name': 'Genre', 'required': False, 'type': 'choice', 'multiple_allowed': True, 'choices': ['Action', 'Science Fiction', 'Superhero', 'Animated', 'Suspense', 'Drama'], 'additions_allowed': True},
-    {'id': 'type', 'name': 'Type', 'required': False, 'type': 'choice', 'multiple_allowed': False, 'choices': ['Movie', 'TV Series', 'TV Miniseries'], 'additions_allowed': False},
-    {'id': 'format', 'name': 'Format', 'required': False, 'type': 'choice', 'multiple_allowed': False, 'choices': ['DVD', 'Blu-Ray DVD', 'MP4'], 'additions_allowed': False},
-    {'id': 'location', 'name': 'Location', 'required': True, 'type': 'choice', 'multiple_allowed': False, 'choices': ['Meteor Room DVD Cabinet', 'Heather\'s iMac', 'Meteor Room DVD Folio'], 'additions_allowed': False},
+    {'id': 'genre', 'name': 'Genre', 'required': False, 'type': 'choice', 'multiple_allowed': True, 'additions_allowed': True,
+         'choices': [
+             'Action', 'Science Fiction', 'Superhero', 'Animated', 'Suspense', 'Drama'], },
+    {'id': 'type', 'name': 'Type', 'required': False, 'type': 'choice', 'multiple_allowed': False, 'additions_allowed': False,
+         'choices': [
+             'Movie', 'TV Series', 'TV Miniseries'], },
+    {'id': 'format', 'name': 'Format', 'required': False, 'type': 'choice', 'multiple_allowed': False, 'additions_allowed': False,
+         'choices': [
+             'DVD', 'Blu-Ray DVD', 'MP4'], },
+    {'id': 'location', 'name': 'Location', 'required': True, 'type': 'choice', 'multiple_allowed': False, 'additions_allowed': False,
+         'choices': [
+             'Meteor Room DVD Cabinet', 'Heather\'s iMac', 'Meteor Room DVD Folio'], },
     ]
 
-attribute_choices = {
-    'genre': {'multiple_allowed': True, 'additions_allowed': True, 'choices': ['Action', 'Science Fiction', 'Superhero', 'Animated', 'Suspense', 'Drama']},
-    'type': {'multiple_allowed': False, 'additions_allowed': False, 'choices': ['Movie', 'TV Series', 'TV Miniseries']},
-    'format': {'multiple_allowed': False, 'additions_allowed': False, 'choices': ['DVD', 'Blu-Ray DVD', 'MP4']},
-    'location': {'multiple_allowed': False, 'additions_allowed': False, 'choices': ['Meteor Room DVD Cabinet', 'Heather\'s iMac', 'Meteor Room DVD Folio']},
+att_ui_stuff = {
+    'name': {'example_text': 'e.g., Tron Legacy'},
+    '': {},
     }
 
+# =============================================================================
+# attribute_rules = { 
+#     'genre': {'multiple_allowed': True, 'additions_allowed': True, 'choices': [
+#         'Action', 'Science Fiction', 'Superhero', 'Animated', 'Suspense', 'Drama']},
+#     'type': {'multiple_allowed': False, 'additions_allowed': False, 'choices': [
+#         'Movie', 'TV Series', 'TV Miniseries']},
+#     'format': {'multiple_allowed': False, 'additions_allowed': False, 'choices': [
+#         'DVD', 'Blu-Ray DVD', 'MP4']},
+#     'location': {'multiple_allowed': False, 'additions_allowed': False, 'choices': [
+#         'Meteor Room DVD Cabinet', 'Heather\'s iMac', 'Meteor Room DVD Folio']},
+#     }
+# =============================================================================
 
-def get_movie_data():
-    return dw.read_data_store(cache_data_filename)
+attribute_dict = {d.get('id'):{**d, **att_ui_stuff.get(d.get('id'), {})} for d in attribute_defs if d.get('id')}
 
-def post_movie_data(data):
-    resp = dw.replace_datastore(cache_data_filename, data)
+multiple_allowed = [d.get('id') for d in attribute_defs if d.get('multiple_allowed')]
+
+def refresh_movie_data():
+    global current_show_data
+    current_show_data = dw.read_data_store(cache_data_filename)
+
+def add_movie(movie_dict):
+    global current_show_data
+    current_show_data.append(movie_dict)
+    update_movie_cache()
+    
+def update_movie_cache():
+    resp = dw.replace_datastore(cache_data_filename, current_show_data)
     if resp:
         return True
     else:
@@ -59,8 +89,8 @@ show_data = show_data_existing if show_data_existing else {}
 
 if not show_data:
     show_data_test = [
-        {'show_type': 'Movie', 'title': 'Donnie Darko', 'year': '2001', 'genre': [], 'format': 'DVD'},
-        {'show_type': 'Movie', 'title': 'Pearl Harbor', 'year': '2001', 'genre': [], },
+        {'type': 'Movie', 'name': 'Donnie Darko', 'year': '2001', 'genre': [], 'format': 'DVD'},
+        {'type': 'Movie', 'name': 'Pearl Harbor', 'year': '2001', 'genre': [], },
         ]
     dw.replace_datastore(cache_data_filename, show_data_test)
 
@@ -73,9 +103,32 @@ app = Flask(__name__)
 def index():
     return 'Python App Server'
 
-@app.route('/appinfo')
-def appinfo():
-    return render_template('appinfo.html')
+@app.route('/dvdjay', methods=['GET', 'POST'])
+def dvdjay():
+    if request.method == 'GET':
+        refresh_movie_data()
+        return render_template('dvdjay.html',
+                               att_defs=attribute_defs,
+                               att_dict=attribute_dict,
+                               shows=current_show_data,
+                               )
+    elif request.method == 'POST':
+        print('FORM:', request.form)
+        action = request.form.get('action')
+        if action == 'new_show':
+            pass
+            new = {k:v for k, v in request.form.items() if v}
+            for att in multiple_allowed:
+                new[att] = request.form.getlist(att)
+            # print('JUST THE FACTS:', new)
+            add_movie(new)
+        elif action == 'new_list_item':
+            listname = request.form.get('list')
+            additem = request.form.get('add_item')
+            print('adding', additem, 'to list', listname)
+            
+        return redirect(request.referrer)
+    
 
 
 #%% GO TIME
